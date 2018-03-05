b59b624141ae51afc2755b4763d1dd6bf662be71
Anthony Vito <anthony.vito@gmail.com>
2018-03-05 09:27:17 -0500

    remove uneeded import

--- src/main/java/com/ethlo/persistence/tools/eclipselink/EclipselinkDdlGenerationMojo.java
+++ src/main/java/com/ethlo/persistence/tools/eclipselink/EclipselinkDdlGenerationMojo.java
@@ -21,7 +21,6 @@ package com.ethlo.persistence.tools.eclipselink;
  */
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
